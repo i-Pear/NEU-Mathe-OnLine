6a8f97841f3f176f9336048f325857bcb61dbdcb
iPear <>
2020-11-09 11:59:36 +0800

    change index

--- src/main/java/com/ipear/web/training/controller/RenderController.java
+++ src/main/java/com/ipear/web/training/controller/RenderController.java
@@ -1,18 +1,14 @@
 package com.ipear.web.training.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RenderController {
 
     @RequestMapping("/")
     public String index() {
-        return "index";
+        return "app";
     }
 
     @RequestMapping("/train")
