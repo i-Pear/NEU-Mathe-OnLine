e93a15ea2b3f153469f5214cce560113f63e2b38
iPear <>
2020-11-30 19:12:53 +0800

    Fix bugs

--- src/main/resources/static/static/js/utils.js
+++ src/main/resources/static/static/js/utils.js
@@ -9,3 +9,21 @@ function queryVariable(variable) {
     }
     return false;
 }
+
+function getCookie(name) {
+    var prefix = name + "="
+    var start = document.cookie.indexOf(prefix)
+
+    if (start === -1) {
+        return null;
+    }
+
+    var end = document.cookie.indexOf(";", start + prefix.length)
+    if (end === -1) {
+        end = document.cookie.length;
+    }
+
+    var value = document.cookie.substring(start + prefix.length, end)
+    return unescape(value);
+}
+
